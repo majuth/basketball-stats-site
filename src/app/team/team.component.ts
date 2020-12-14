569ed30576d29dfc64faae6197380067b8b544e8
Majuth <majuth101@hotmail.com>
2020-12-13 23:11:38 -0500

    Loaded team game data

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { HttpClient } from '@angular/common/http';
+import { game, gameData } from '../game';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-team',
@@ -13,6 +15,7 @@ export class TeamComponent implements OnInit {
 
   teamID:number;
   teamData: Array<string>;
+  teamGames: Array<game>;
 
   ngOnInit() {
     this.data.currentTeam.subscribe((team) => {this.teamID = team; this.loadTeamData()});
@@ -22,15 +25,34 @@ export class TeamComponent implements OnInit {
     var teamData= [];
     
     this.http.get("https://www.balldontlie.io/api/v1/teams/" + this.teamID).toPromise().then(data =>{
-      console.log(data);
 
         for(let key in data)
           if (data.hasOwnProperty(key))
           teamData.push(data[key]);
 
-        console.log(teamData);
 
         this.teamData = teamData;
-  });}
+
+        
+  });
+
+  this.loadTeamGames();
+  }
+  
+  loadTeamGames(){
+    var startDate = new Date();
+
+    startDate.setMonth(startDate.getMonth() - 10);
+    
+    var dateString = startDate.getFullYear() + "-" + startDate.getMonth() + "-" + startDate.getDate();
+    this.getGameData(dateString).subscribe( res => this.teamGames = res.data.sort((a,b) => b.date.localeCompare(a.date)));
+    console.log(this.teamGames);
+
+  }
+
+  getGameData(dateString: string): Observable<gameData>{
+    return this.http.get<gameData>("https://www.balldontlie.io/api/v1/games?team_ids[]=" + this.teamID + "&start_date=%27"+ dateString +"%27&per_page=100");
+  }
+
 
 }
